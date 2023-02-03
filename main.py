6579dae7c48633021798d2a1a2057ce336af2584
nilsonbsr <niloobassir@gmail.com>
2023-02-03 12:58:35 +0100

    adding graphics and audio folders to porject

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 # clock object helps us with time and frame rate
 clock = pygame.time.Clock()
-# display surface and regular surface (screen) has lots of in common and we display it in while True 
+# display surface and regular surface (screen) has lots of in common and we display it in while True
 test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red ')
 
 
 while True:
@@ -23,5 +24,8 @@ while True:
     # draw all our elements
     # update everything
     pygame.display.update()
+    # attach test_surface to display surface
+    # blit stands for block instance transfer it takes 2 arguments the surface we want to place and the position
+    screen.blit(test_surface, (200,100))
     # this 60 tells that this while True loop should not run faster than 60 fps
     clock.tick(60)
